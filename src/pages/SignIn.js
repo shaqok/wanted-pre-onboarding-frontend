8023367585a22f1383b17e564e28c0754618734e
shaqok <shaqok8@gmail.com>
2023-04-03 21:54:09 +0900

    Implemented SignIn Page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import LoginForm from '../components/LoginForm';
+import { useNavigate } from 'react-router-dom';
+import { makeRequest } from '../utils/makeRequest';
 
 const SignIn = () => {
+  const navigate = useNavigate();
+
+  const onSignIn = async (email, password) => {
+    const body = { email, password }
+    const response = await makeRequest('/auth/signin', 'post', body, false);
+    if (response.status === 200) {
+      localStorage.setItem('access_token', response.data.access_token);
+      navigate('/todo');
+    }
+  }
+
   return (
-    <div>SignIn</div>
+    <div>
+      <LoginForm onSubmit={onSignIn} buttonText='SignIn' />
+    </div>
   )
 }
 

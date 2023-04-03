2f1bee5c111cebd556c52bcd5b4a659bae4cd166
shaqok <shaqok8@gmail.com>
2023-04-03 21:53:57 +0900

    Implemented SignUp page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import LoginForm from '../components/LoginForm'
+import { useNavigate } from 'react-router-dom'
+import { makeRequest } from '../utils/makeRequest'
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  const onSignUp = async (email, password) => {
+    const body = { email, password }
+    const response = await makeRequest('/auth/signup', 'post', body, false);
+    if (response.status === 201) {
+      navigate('/signin');
+    }
+  }
+
   return (
-    <div>SignUp</div>
+    <div>
+      <LoginForm onSubmit={onSignUp} buttonText='SignUp' />
+    </div>
   )
 }
 

4d7b8658631b19d928e532e1c03a5e07db3c3a60
shaqok <shaqok8@gmail.com>
2023-04-04 00:51:30 +0900

    - Implemeted getTodos - Implemented createTodo - Implemented updateTodo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TodoItem from './TodoItem'
 import '../App.css'
-
-const mockData = ['todo 1', 'todo 2', 'todo 3']
+import { makeRequest } from '../utils/makeRequest'
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [todoInput, setTodoInput] = useState('');
+
+  const createTodo = async () => {
+    const response = await makeRequest('/todos', 'post', { todo: todoInput}, true);
+    if (response.status === 201) {
+      setTodos(todos.concat({...response.data}))
+    }
+    setTodoInput('');
+  }
+
+  const updateTodo = async (data) => {
+    const { id, todo, isCompleted } = data;
+    const response = await makeRequest(`/todos/${id}`, 'put', { todo, isCompleted: !isCompleted }, true);
+    if (response.status === 200) {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          todo.todo = response.data.todo;
+          todo.isCompleted = response.data.isCompleted;
+        }
+        return todo;
+      })
+      setTodos(newTodos)
+    }
+  }
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const response = await makeRequest('/todos', 'get', null, true);
+      if (response.status === 200) {
+        setTodos(todos.concat(response.data));
+      }
+    }
+
+    getTodos();
+  }, [])
+  
+
   return (
     <div className='todo-container'>
+      <div>
+          
+        <input 
+          data-testid="new-todo-input" 
+          value={todoInput} 
+          onChange={(event) => setTodoInput(event.target.value)} 
+        />
+        <button data-testid="new-todo-add-button" onClick={createTodo}>추가</button>
+      </div>
+
       <ul className='todo-ul'>
-        {mockData.map((todo, i) => <TodoItem key={i} content={todo} />)}
+        {todos && todos.map((data) => 
+          <TodoItem key={data.id} data={data} updateTodo={updateTodo} />
+        )}
       </ul>
     </div>
   )

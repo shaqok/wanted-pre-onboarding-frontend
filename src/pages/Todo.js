a6ed88f9c728370fad94077b623bd9efd9fdaf33
shaqok <shaqok8@gmail.com>
2023-04-03 22:41:38 +0900

    Handle redirection from /todo to /signin if access token doesn't exist

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import TodoList from '../components/TodoList';
 
 const Todo = () => {
+  const navigate = useNavigate();
+  const [showTodo, setShowTodo] = useState(false);
+
+  useEffect(() => {
+    const access_token = localStorage.getItem('access_token');
+    if (!access_token) {
+      navigate('/signin');
+    } else {
+      setShowTodo(true);
+    }
+  }, [navigate])
+  
+
   return (
-    <div>Todo</div>
+    <div>
+      {showTodo && <TodoList />}
+    </div>
   )
 }
 
